17dd841363196cdafa9f863043e4cc5451f01871
PhatCuong <cuongphat123bt@gmail.com>
2024-04-26 15:11:21 +0700

    Update files

--- src/test/java/StepDefinitions/LoginStepDef.java
+++ src/test/java/StepDefinitions/LoginStepDef.java
@@ -2,6 +2,9 @@ package StepDefinitions;
 
 import io.cucumber.java.en.*;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class LoginStepDef {
 
@@ -9,6 +12,14 @@ public class LoginStepDef {
 
     @Given("user is on MarAir homepage")
     public void user_is_on_MarAir_homepage(){
+        System.setProperty("webdriver.chrome.driver", "/Users/user/Documents/Automation/MarAirAutomation/MarsAir/src/test/resources/Driver/chromedriver");
+
+        driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+
+//        driver.manage().window().maximize();
 
         System.out.println("User is at MarAir homepage");
     }
